12f41b2f43e48cafb51503be28745216a1bf0dbb
Elizabeth Larson <elarso2@uw.edu>
2022-04-01 11:37:21 -0700

    readme updated with image and link, more fetch function lines added to js

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-This weeks assignment is to create a weather dashboard, which allows a user to input a city and retrieve weather data for that city. It utilizes third party API's
+This weeks assignment is to create a weather dashboard, which allows a user to input a city and retrieve weather data for that city. It utilizes two third party API's, one is a Geocoding API which returns the latitude and longitude of the city a user enters, and the other is a daily weather API which uses that latitude and longitude response to pull the current and future weather for the user's selected city.
+
+We were given the following image as a guide for our page:
+
+![example photo](./assets/homework-demo.png)
+
+And my live webpage can be found [here](https://elarso2.github.io/WeatherDashboard/).
--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,15 @@
 var searchBtn = document.getElementById("searchBtn");
+var cityInputEl = document.getElementById("citySearch");
 
 // function that takes and stores user input, and then converts the city into a latitude and longitude, to be entered into the weather API function.
-function latLonApi() {}
+function latLonApi() {
+  var latUrl =
+    "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit=1&appid=ce90f4792e1731721676f29be383bfdb";
+
+  fetch(latUrl).then(function (response) {
+    return Response.json();
+  });
+}
 
 // function to use lat and long from above, and pull weather data using the One Call Weather API.
 function weatherApi() {
@@ -9,14 +17,22 @@ function weatherApi() {
     "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude=daily&appid=ce90f4792e1731721676f29be383bfdb";
 
   fetch(weatherUrl)
-    .then(function (respone) {
+    .then(function (response) {
       return Response.json();
     })
     .then(function (data) {
       for (var i = 0; i < data.length; i++) {
-        console.log(data[i].daily.temp);
+        //for the current day's temp
+        console.log(data[i].current.weather.icon);
+        console.log(data[i].current.temp);
+        console.log(data[i].current.wind_speed);
+        console.log(data[i].humidity);
+        console.log(data[i].uvi);
+        //for future forecast dates
+        console.log(data[i].daily.weather.icon);
+        console.log(data[i].daily.temp.day);
         console.log(data[i].daily.humidity);
-        console.log(data[i].daily.uvi);
+        // console.log(data[i].daily.uvi);
         console.log(data[i].daily.wind_speed);
       }
     });
