93bbdd186626c994a5edaa6d5eac798e0eb6ff33
Elizabeth Larson <elarso2@uw.edu>
2022-04-01 08:18:50 -0700

    fetch function additional code for weather API with console logging the desired data

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,26 @@
 var searchBtn = document.getElementById("searchBtn");
 
 // function that takes and stores user input, and then converts the city into a latitude and longitude, to be entered into the weather API function.
-function latLonApi(){
-
-}
-
+function latLonApi() {}
 
 // function to use lat and long from above, and pull weather data using the One Call Weather API.
 function weatherApi() {
-    var weatherUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=ce90f4792e1731721676f29be383bfdb';
+  var weatherUrl =
+    "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude=daily&appid=ce90f4792e1731721676f29be383bfdb";
 
-    fetch(weatherUrl)
-    .then(function (respone){
-        return Response.json();
+  fetch(weatherUrl)
+    .then(function (respone) {
+      return Response.json();
     })
     .then(function (data) {
-        for 
-    })
+      for (var i = 0; i < data.length; i++) {
+        console.log(data[i].daily.temp);
+        console.log(data[i].daily.humidity);
+        console.log(data[i].daily.uvi);
+        console.log(data[i].daily.wind_speed);
+      }
+    });
 }
 
 //using the search button to activate the latlonfxn
-searchBtn.addEventListener('click', latLonApi);
+searchBtn.addEventListener("click", latLonApi);
