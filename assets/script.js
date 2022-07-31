3b2eb436ca99870e4f1b9e90f366e1995374d168
Elizabeth Larson <elarso2@uw.edu>
2022-07-30 20:55:59 -0700

    adjusting html with new bootstrap, starting js edits

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,33 @@
 var searchBtn = document.getElementById("searchBtn");
-var cityInputEl = document.getElementById("citySearch");
+var cityInputEl = document.getElementById("citySearch").value;
+var cityNameDisplay = document.getElementById("cityName");
+
+function cityName() {
+  console.log(cityInputEl);
+}
 
 // function that takes and stores user input, and then converts the city into a latitude and longitude, to be entered into the weather API function.
 function latLonApi() {
   var latUrl =
     "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit=1&appid=ce90f4792e1731721676f29be383bfdb";
 
-  fetch(latUrl).then(function (response) {
-    return Response.json();
-  });
+  fetch(latUrl)
+    .then(function (response) {
+      return Response.json();
+    })
+    .then(function (data) {
+      getWeather(data[0].lat, data[0].lon);
+    });
 }
 
 // function to use lat and long from above, and pull weather data using the One Call Weather API.
 function weatherApi() {
   var weatherUrl =
-    "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude=daily&appid=ce90f4792e1731721676f29be383bfdb";
+    "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+    latitude +
+    "&lon=" +
+    longitude +
+    "&exclude=daily&appid=ce90f4792e1731721676f29be383bfdb";
 
   fetch(weatherUrl)
     .then(function (response) {
@@ -39,4 +52,4 @@ function weatherApi() {
 }
 
 //using the search button to activate the latlonfxn
-searchBtn.addEventListener("click", latLonApi);
+searchBtn.addEventListener("click", cityName);
